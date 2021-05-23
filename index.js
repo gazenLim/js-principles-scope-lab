b6cc92c668b593034eea2635d4c174c9a97e1234
gajen <meetgajen@yahoo.com>
2021-05-23 09:33:38 -0400

    commit tree acb3d263dd1f7a0c5d1729d861f608d412807579

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'dan'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(newFavorite){
+    bestCustomer = newFavorite
+
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "dan"
+}
